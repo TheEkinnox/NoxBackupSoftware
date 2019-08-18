8a6d141cd78ca265496e306bd990ed6d7fbd9901
Loïck Noa OBIANG NDONG <theekinnox@gmail.com>
2019-08-18 11:01:54 -0400

    Début du désign de l'application

---
+++ NoxBackupSoftware/frmPrincipal.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace NoxBackupSoftware
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAjouter = new System.Windows.Forms.Button();
+            this.btnModifier = new System.Windows.Forms.Button();
+            this.grpSauvegardes = new System.Windows.Forms.GroupBox();
+            this.btnSupprimer = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.grpInfos = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.grpSauvegardes.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.grpInfos.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnAjouter
+            // 
+            this.btnAjouter.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnAjouter.Location = new System.Drawing.Point(7, 408);
+            this.btnAjouter.Name = "btnAjouter";
+            this.btnAjouter.Size = new System.Drawing.Size(86, 23);
+            this.btnAjouter.TabIndex = 2;
+            this.btnAjouter.Text = "Ajouter";
+            this.btnAjouter.UseVisualStyleBackColor = true;
+            // 
+            // btnModifier
+            // 
+            this.btnModifier.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnModifier.Location = new System.Drawing.Point(99, 408);
+            this.btnModifier.Name = "btnModifier";
+            this.btnModifier.Size = new System.Drawing.Size(86, 23);
+            this.btnModifier.TabIndex = 3;
+            this.btnModifier.Text = "Modifer";
+            this.btnModifier.UseVisualStyleBackColor = true;
+            // 
+            // grpSauvegardes
+            // 
+            this.grpSauvegardes.Controls.Add(this.btnSupprimer);
+            this.grpSauvegardes.Controls.Add(this.listBox1);
+            this.grpSauvegardes.Controls.Add(this.btnModifier);
+            this.grpSauvegardes.Controls.Add(this.btnAjouter);
+            this.grpSauvegardes.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grpSauvegardes.Location = new System.Drawing.Point(0, 0);
+            this.grpSauvegardes.Name = "grpSauvegardes";
+            this.grpSauvegardes.Size = new System.Drawing.Size(283, 441);
+            this.grpSauvegardes.TabIndex = 4;
+            this.grpSauvegardes.TabStop = false;
+            this.grpSauvegardes.Text = "Mes sauvegardes";
+            // 
+            // btnSupprimer
+            // 
+            this.btnSupprimer.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSupprimer.Location = new System.Drawing.Point(191, 408);
+            this.btnSupprimer.Name = "btnSupprimer";
+            this.btnSupprimer.Size = new System.Drawing.Size(86, 23);
+            this.btnSupprimer.TabIndex = 4;
+            this.btnSupprimer.Text = "Supprimer";
+            this.btnSupprimer.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(7, 18);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(270, 381);
+            this.listBox1.TabIndex = 0;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.MinimumSize = new System.Drawing.Size(784, 441);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.grpSauvegardes);
+            this.splitContainer1.Panel1MinSize = 283;
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.grpInfos);
+            this.splitContainer1.Panel2MinSize = 283;
+            this.splitContainer1.Size = new System.Drawing.Size(784, 441);
+            this.splitContainer1.SplitterDistance = 283;
+            this.splitContainer1.TabIndex = 5;
+            // 
+            // grpInfos
+            // 
+            this.grpInfos.Controls.Add(this.textBox1);
+            this.grpInfos.Controls.Add(this.label1);
+            this.grpInfos.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grpInfos.Location = new System.Drawing.Point(0, 0);
+            this.grpInfos.Name = "grpInfos";
+            this.grpInfos.Size = new System.Drawing.Size(497, 441);
+            this.grpInfos.TabIndex = 0;
+            this.grpInfos.TabStop = false;
+            this.grpInfos.Text = "Informations de la sauvegarde";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nom de la sauvegarde :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(134, 18);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(351, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // FrmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 441);
+            this.Controls.Add(this.splitContainer1);
+            this.MinimumSize = new System.Drawing.Size(800, 480);
+            this.Name = "FrmPrincipal";
+            this.Text = "Nox Backup Software";
+            this.grpSauvegardes.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.grpInfos.ResumeLayout(false);
+            this.grpInfos.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnAjouter;
+        private System.Windows.Forms.Button btnModifier;
+        private System.Windows.Forms.GroupBox grpSauvegardes;
+        private System.Windows.Forms.Button btnSupprimer;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.GroupBox grpInfos;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
